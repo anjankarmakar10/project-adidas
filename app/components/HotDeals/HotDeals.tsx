2f4df9773492f90476a2042d2c57cb336f387b39
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-08-14 08:50:56 +0600

    Build Features sections

--- app/components/HotDeals/HotDeals.tsx
+++ app/components/HotDeals/HotDeals.tsx
@@ -2,6 +2,23 @@ import { Button } from "@/components/ui/button";
 import ProductCard from "../Cards/ProductCard";
 
 const HotDeals = () => {
-  return <section className="container px-4 mx-auto py-10"></section>;
+  return (
+    <section className="container px-4 mx-auto py-10">
+      <header className="font-bold text-3xl mb-6 flex items-center justify-between">
+        Featured products
+        <Button>View all</Button>
+      </header>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </section>
+  );
 };
 export default HotDeals;
